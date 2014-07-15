3e34a86b121346f181ff4b0a0d707ba0df1ee736
Chris Reeves <chris@ev-soft.net>
2014-07-15 13:53:32 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+ractive-delayed-input - Ractive input field that waits for a timeout before firing a change
+================================================================
+
+component
+---------
+This component assumes use of the [component](http://component.io/) web component system.
+
+Example
+-------
+To try the example, change the ractive dependency in component.json to something that exists and that is greater than 0.4.0, run ```component build```, and open example.html in your browser.
---
+++ index.js
@@ -0,0 +1,38 @@
+var Ractive = require('ractive');
+
+var DelayedInput = Ractive.extend({
+  template: '{{>element}}',
+  beforeInit: function(opts) {
+    var template = '<input value="{{.value}}" on-keydown="timeout-update"| />';
+    var copy = ['placeholder', 'class', 'id', 'style'];
+    var out = '';
+    for (var k in opts.data) if (k.indexOf('on-') === 0 || copy.indexOf(k) > -1) out += k + '="' + opts.data[k] + '"';
+    opts.partials.element = template.replace(/\|/, out);
+  },
+  lazy: true,
+  init: function() {
+    var me = this;
+    me.on('timeout-update', function(e) {
+      var k = e.original.keyCode;
+      if (k !== 8 && k !== 32 && k !== 46 && k < 48) {
+        me.fire('key', e);
+        return;
+      }
+      if (!!me.timer) {
+        clearTimeout(me.timer);
+        me.timer = null;
+      }
+      if (e.node.value !== me.get('value')) {
+        me.timer = setTimeout(function() {
+          me.fire('changed', e.node.value, e);
+        }, me.get('timeout'));
+      }
+    });
+  },
+  data: {
+    timeout: 500
+  },
+  partials: { element: '' }
+});
+
+module.exports = DelayedInput;
